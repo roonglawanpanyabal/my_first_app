461420babd2734d6618b7a108be9d11ff109191b
roonglawanpanyabal <54173@yupparaj.ac.th>
2026-07-23 09:13:58 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -5,8 +5,8 @@ st.title("🛒แอปพลิเคชั่นคำนวณราคาส
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 vat = price * 0.07
 
+st.header("ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}**บาท")
 net_price = price - vat
-vat = price * 0.07
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 
 st.divider()
