8eea5aba3cfb6fdc4e4bd4b01bfe446db1ebea8a
roonglawanpanyabal <54173@yupparaj.ac.th>
2026-09-03 08:40:51 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -7,14 +7,17 @@ st.write("กรอกข้อมูลน้ำหนักและส่ว�
 #ส่วนที่ 2 สร้างช่องรับค่าน้ำหนัก และ ส่วนสูง
 weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):", min_value=1.0)
 height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0)
+
+
 if st.button("คำนวณค่า BMI 🎯"):
     # แปลงส่วนสูงจาก cm เป็นเมตร แล้วคำนวณ BMI
     height_m = height_cm / 100
     bmi = weight / (height_m ** 2)
-
-    st.write("---")
+   
+    st.write("---")  
     st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
-    #ส่วนที่ 4 แปลผลค่า BMI ตามเกณฑ์
+
+#ส่วนที่ 4 แปลผลค่า BMI ตามเกณฑ์
 if bmi < 18.5:
     st.warning("⚠️ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
 elif 18.5 <= bmi < 23.0:
