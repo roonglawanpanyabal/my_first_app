548f3caa07d094992386d14b487b58dc32d5d780
roonglawanpanyabal <54173@yupparaj.ac.th>
2026-09-03 09:06:58 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -39,7 +39,7 @@ def show_result_dialog(ans1, ans2):
   # ตรวจข้อ 2
     if u_ans2 == "fish":
        st.success(f"✅ ข้อ 2: ถูกต้อง")
-       score += 1
+       score +- 1
     else:
        st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ {u_ans2})")
 
@@ -47,7 +47,7 @@ def show_result_dialog(ans1, ans2):
 
 st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score -= 2:
+    if score == 2:
        st.success("🎉 You win!")
     else:
        st.error("😢 You lose!")
